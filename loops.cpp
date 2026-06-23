67dfbe16149ec8aedf69c883f6bda5dd86d7d9fb
Rajarshi Karmakar <rajarshikarmakar123@gmail.com>
2026-06-23 21:02:42 +0530

    Learnt about forloop, while loop and do while

---
+++ loops.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i;
+    for (i =1; i<=5; i = i+1){
+        cout <<"Rishi" << i << endl;
+    }
+
+    int j;
+    for (j=1; j<50; j=j+10){
+        cout << "Siddharth" << j << endl;
+    }
+
+    int k = 1;
+    while(k<50){
+        cout<<"Hi this is WHILE LOOP" << k << endl;
+        k = k*5;}
+
+        //do while loop makes sure that the program runs a minimum of one time
+
+    int m = 2;
+    do{
+        cout << "Hi this is Do while" << m << endl;
+        m = m+1;
+    } while(m<=1);
+
+    cout << m << endl;
+
+    return 0;
+
+
+
+}
