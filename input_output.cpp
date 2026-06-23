ba75eb20ac82235eb50ae9582ac88cf447f4315b
Rajarshi Karmakar <rajarshikarmakar123@gmail.com>
2026-06-23 11:47:10 +0530

    Learnt How to take inputs in CPP

---
+++ input_output.cpp
@@ -0,0 +1,20 @@
+#include<iostream> //for taking inputs and outputs we have to include  the iostream library
+#include<bits/stdc++.h> //includes all libraries in cpp
+using namespace std; //using std for every function 
+
+
+
+int main(){
+    int x,y;
+    cout << "Enter x and y: ";
+    cin >> x >>y;
+    cout <<"Value of x:" << x << "\n" << "Value of y is:" << y;
+    return 0;
+}
+
+
+
+
+
+
+
