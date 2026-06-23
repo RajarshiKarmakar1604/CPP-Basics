d6fd59144ab683943ffdda36d07343f264bc621b
Rajarshi Karmakar <rajarshikarmakar123@gmail.com>
2026-06-23 13:54:58 +0530

    learnt strings in cpp

---
+++ strings.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    string s = "Striver";
+    int len = s.size();
+    s[len-1]='m';
+    cout<<s[len-1]<<"\n";
+    cout<< s ;
+
+    return 0;
+}
