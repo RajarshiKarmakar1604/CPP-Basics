a3bb5b2fef0b135fac1e66268b878bc282fe6811
Rajarshi Karmakar <rajarshikarmakar123@gmail.com>
2026-06-23 11:29:22 +0530

    Learnt Basics of CPP including Headers

---
+++ cpp_basics.cpp
@@ -0,0 +1,28 @@
+#include<iostream> //for taking inputs and outputs we have to include  the iostream library
+#include<math.h>
+#include<string>
+using namespace std; //using std for every function 
+
+int main(){
+    cout << "Hey Rishi \n";
+    cout << "I am Siddharth";
+    return 0;
+}
+
+
+
+
+
+
+
+/*
+
+int main(){
+    std::cout << "Hey Rishi \n";
+    std::cout << "I am Siddharth";
+    return 0;
+}
+
+without std 
+
+*/
