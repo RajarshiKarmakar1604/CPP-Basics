de7e62131d89a9eb05e22e72e767f1d1e46b7bec
Rajarshi Karmakar <rajarshikarmakar123@gmail.com>
2026-06-24 19:09:12 +0530

    Finished Day 2 -  16 Pattern Problems

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -11,8 +11,6 @@ void print1(int n){
         cout << endl;
     }
 }
-
-
 void print2(int n){
     for (int i=0; i<n; i++){
         for(int j=0; j<=i ;j++){
@@ -21,8 +19,6 @@ void print2(int n){
     cout<<endl;
     }
 }
-
-
 void print3(int n){
     for (int i=1; i<=n; i++){
         for(int j=1; j<=i ;j++){
@@ -31,8 +27,6 @@ void print3(int n){
     cout<<endl;
     }
 }
-
-
 void print4(int n){
     for (int i=1; i<=n; i++){
         for(int j=1; j<=i ;j++){
@@ -41,7 +35,6 @@ void print4(int n){
     cout<<endl;
     }
 }
-
 void print5(int n){
     for (int i=1; i<=n; i++){
         for (int j=0; j<(n-i+1); j++){
@@ -50,8 +43,6 @@ void print5(int n){
         cout<<endl; 
     }    
 }
-
-
 void print6(int n){
     for (int i=1; i<=n; i++){
         for (int j=0; j<(n-i+1); j++){
@@ -60,7 +51,6 @@ void print6(int n){
         cout<<endl; 
     }    
 }
-
 void print7(int n){
     for (int i=1; i<=n; i++){
         for (int j=0; j<(n-i+1); j++){
@@ -69,7 +59,6 @@ void print7(int n){
         cout<<endl; 
     }    
 }
-
 void print8(int n){
     for (int i=0; i<n; i++){
         //space
@@ -88,7 +77,6 @@ void print8(int n){
     }
 
 }
-
 void print9(int n){
     for(int i=0; i<n; i++){
         //space
@@ -107,7 +95,6 @@ void print9(int n){
         cout << endl;
     }   
 }
-
 void print10(int n){
     
     for (int i=0; i<n; i++){
@@ -142,13 +129,100 @@ void print10(int n){
         cout << endl;
     }   
 
+}
+void print11(int n){
+    //stars one
+    for (int i=1; i<=n; i++){
+        
+        for (int j=0; j<i; j++){
+            cout<<"*";
 
+        }
+        cout<<endl;
+    }
 
-
-
+    for (int i =1; i<=n; i++){
+        for (int j=0; j<n-i+1; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
 
 
 }
+void print12(int n){
+    int start = 1;
+    for (int i = 0; i<n;i++){
+        if (i%2==0){
+            start = 1;
+        }
+        else{
+            start =0;
+        }
+
+        for (int j = 0; j<=i; j++){
+            cout <<start<< " ";
+            start = 1-start;
+        }
+        cout << endl;
+    }
+}
+
+// void print13(int n){
+//     int space = 2*(n-1);
+//     for(int i =1; i<=n;i++){
+//         for(int j=1; j<=i;j++){
+//             cout<<j;
+//         }
+//         for (int j =1; j<=space;j++){
+//         cout<<" ";
+//     }
+//         for(int j=1; j<=i;j++){
+//             cout<<j;
+//         }
+//         cout <<endl;
+//         space = space-2;
+//     }
+// }
+
+void print14(int n){
+        int num = 1;
+        for (int i = 1; i<=n; i++){
+            for (int j =1; j<=i; j++){
+                cout << num << " ";
+                num = num+1;
+            }
+            cout << endl;
+        }
+}
+void print15(int n){
+    for (int i = 0; i<=n; i++){
+        for(char ch = 'A'; ch<'A'+i; ch++){
+            cout<<ch <<" ";
+        }
+        cout <<endl;
+    }
+}
+void print16(int n){
+    for (int i = 0; i<=n; i++){
+        for(char ch = 'A'; ch<='A'+n-i-1; ch++){
+            cout<<ch <<" ";
+        }
+        cout <<endl;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // using test cases
 
@@ -160,6 +234,6 @@ int main(){
         cout<<"Enter number of stars to print:";
         int n;
         cin >> n;
-        print10(n);
+        print16(n);
     }
 }
