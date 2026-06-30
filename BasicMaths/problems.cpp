68fc2e79610c052625afebe151471d757e0f90a6
Rajarshi Karmakar <rajarshikarmakar123@gmail.com>
2026-07-01 00:17:11 +0530

    Learnt some more STL functions and solved 2 more Math problems

--- BasicMaths/problems.cpp
+++ BasicMaths/problems.cpp
@@ -23,8 +23,7 @@ void problem2(int n){
         n=n/10;
 
     }
-    cout<<"The reverse of the number " << n_copy << " is: " << revNum;
-}
+    cout<<"The reverse of the number " << n_copy << " is: " << revNum;}
 void problem3(int n){
     int revNum = 0;
     int n_copy = n;
@@ -42,8 +41,68 @@ void problem3(int n){
     }
     
 }
+void problem4(int n){
+    int n_copy = n;
+    int dup = n;
+    int sum = 0;
+    int count = 0;
+    int ld;
+    if(n == 0){
+            cout<<"True"<<endl;
+            return;
+    }
+    while(n>0){
+        n/=10;
+        count += 1;
+    }
+    while (n_copy>0){
+        ld = n_copy%10;
+        n_copy/=10;
+        sum += pow(ld, count);
+    }
+    if (sum == dup){
+        cout <<"True"<<endl;
+    }
+    else{
+        cout<<"False"<<endl;
+    }
+    
+       
+}
+void problem5(int n){
+    vector <int> divisor;
+    if(n==0){
+        cout<<"Every integer is a divisor of 0"<<endl;
+        return;
+    }
+    // approach 1 
+    /*
+    for(int i=1; i<=n; i++){
+        if(n%i==0){
+            divisor.emplace_back(i);
+        }
+    }
+    for(auto it:divisor){
+        cout << it << endl;
+    }
+    */
 
-
+    //appraoch 2
+    for (int i = 1; i<=sqrt(n); i++){
+        if(n%i==0){
+            divisor.emplace_back(i);
+            if(i!=n/i){
+                divisor.emplace_back(n/i);
+            }
+        }
+    }
+    sort(divisor.begin(), divisor.end());
+    for(auto it:divisor){
+        cout << it << endl;
+    }
+    
+    
+}
 
 
 
@@ -57,6 +116,6 @@ int main(){
         cout<<"Enter Integer n:";
         int n;
         cin >> n;
-        problem3(n);
+        problem5(n);
     }
 }
---
+++ stl/stl_extras.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explainExtras(){
+    // sorting vectors and arrays
+    vector<int> v;
+    int Arr[6] = {1,4,2,9,1,41};
+    v.emplace_back(1);
+    v.emplace_back(4);
+    v.emplace_back(2);
+    v.emplace_back(144); // vector now is {1,4,2,144} 
+    (0, 6); // for arrays { 1,1,2,4,9,41}
+    sort(v.begin(), v.end()); //[start, end), now array is {1,2,4,144}
+
+    sort(a, a+n; greatest<int>);    //descending order sort
+
+}
+
+
+
+
+
+int main(){
+    explainExtras();
+    return 0;
+
+}
